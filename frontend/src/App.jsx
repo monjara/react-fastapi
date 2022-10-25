c9cba15c5982e80704ceda4c8bce14ba38daa515
monjara <yaginumee@gmail.com>
2022-10-25 19:26:03 +0900

    pnpm

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<p>asdf</p>}/>
+    </Routes>
+  )
+}
+
+export default App
+
